1530558f2e6d26d3bdbdac8e332323ddf008e8cd
DEVILKING13461 <ayaan2882299@gmail.com>
2024-07-24 15:42:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7269742578:AAF2tJBlmBTU83BwfIY8eyeuEk6Gn8yxgtg")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Devil King")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6476377115")
 
 
 
